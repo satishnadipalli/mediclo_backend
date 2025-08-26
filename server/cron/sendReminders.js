5c95805159b826743e306c590f66e59c6c2128ae
satishnadipalli <satishnadipalli1@gmail.com>
2025-08-26 13:47:53 +0530

    updated

--- server/cron/sendReminders.js
+++ server/cron/sendReminders.js
@@ -5,7 +5,7 @@ const { sendAppointmentReminder } = require("../services/whatsapp");
 const sendReminders = () => {
   // Run every day at 1:20 PM (local timezone can be set below)
   cron.schedule(
-    "43 13 * * *",
+    "50 13 * * *",
     async () => {
       console.log("⏰ Cron job triggered at:", new Date().toString());
 
