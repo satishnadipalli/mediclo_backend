4660fe9feb2eb8dcf680cf6663aae076dfca0767
satishnadipalli <satishnadipalli1@gmail.com>
2025-09-02 11:38:46 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -189,12 +189,10 @@ app.get("/", (req, res) => {
 // Error handler
 app.use(errorHandler);
 
-const PORT = 5000;
-
-const server = app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
